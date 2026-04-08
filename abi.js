de5fa89d9dc4f5206a68620e14e0b84da987c4e4
kaderv2 <113213261+kaderv2@users.noreply.github.com>
2026-04-08 11:51:54 +0100

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,10 @@
+const CONTRACT_ABI = [
+ "function deposit(uint256 amount,address referrer)",
+ "function withdraw()",
+ "function compound()",
+ "function withdrawPrincipal()",
+ "function claimReferral()",
+ "function getUserStats(address) view returns(uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256)",
+ "function getPoolStats() view returns(uint256,uint256,uint256,uint256)",
+ "function pendingRewards(address) view returns(uint256)"
+];
